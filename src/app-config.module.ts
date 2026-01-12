b77eb873fa7c9267fd13ceac56061a5f3969b2ba
iusmanof <iusmanof@gmail.com>
2026-01-12 08:21:07 +0300

    feat: v2.1 cross-env NODE_ENV=

--- src/app-config.module.ts
+++ src/app-config.module.ts
@@ -2,6 +2,9 @@ import { ConfigModule } from '@nestjs/config';
 
 export const AppConfigModule = ConfigModule.forRoot({
   isGlobal: true,
-  envFilePath: [`.env.${process.env.NODE_ENV}`],
-  // envFilePath: [`.env.${process.env.NODE_ENV ?? 'development'}`],
+  envFilePath: [
+    `.env.${process.env.NODE_ENV}.local`,
+    `.env.${process.env.NODE_ENV}`,
+    `.env.production`,
+  ].filter(Boolean),
 });
